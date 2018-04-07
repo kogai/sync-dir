37b909ad879f8c5123314799e58dc800aa6eca58
Shinichi Kogai <kogai0121@gmail.com>
2018-04-07 19:40:33 +0900

    enumulate files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::path::Path;
-use std::fs::{copy, read_dir, DirEntry, File};
+extern crate im;
 
-fn open_file(d: std::io::Result<DirEntry>) -> std::io::Result<()> {
+use std::io::Result;
+use std::path::{Path, PathBuf};
+use std::fs::{copy, read_dir, DirEntry, File};
+use im::HashMap;
+
+fn open_file(d: std::io::Result<DirEntry>) -> Result<()> {
     let dir_entry = try!(d);
     let file = try!(File::open(dir_entry.path()));
     let meta = try!(dir_entry.metadata());
@@ -11,16 +15,40 @@ fn open_file(d: std::io::Result<DirEntry>) -> std::io::Result<()> {
     Ok(())
 }
 
+fn correct<P: AsRef<Path>>(path: P) -> HashMap<PathBuf, DirEntry> {
+    match read_dir(path) {
+        Ok(entries) => entries
+            .fold(
+                Ok(HashMap::new()),
+                |acc: Result<HashMap<PathBuf, DirEntry>>, d| {
+                    let dir_entry = try!(d);
+                    let file_type = try!(dir_entry.file_type());
+                    let result = try!(acc);
+                    if file_type.is_dir() {
+                        Ok(result.union(&correct(dir_entry.path())))
+                    } else {
+                        Ok(result.insert(dir_entry.path(), dir_entry))
+                    }
+                },
+            )
+            .unwrap(),
+        Err(err) => unreachable!(err),
+    }
+}
+
 fn main() {
-    let my_dir = read_dir("./fixture/a");
-    match my_dir {
-        Ok(dir) => {
-            dir.for_each(|dir_entry| {
-                open_file(dir_entry);
-            });
-        }
-        Err(x) => {
-            println!("{}", x);
-        }
-    };
+    let a = correct("./fixture/a");
+    let b = correct("./fixture/b");
+    println!("{:?}", a);
+    println!("{:?}", b);
+    // match my_dir {
+    //     Ok(dir) => {
+    //         dir.for_each(|dir_entry| {
+    //             open_file(dir_entry);
+    //         });
+    //     }
+    //     Err(x) => {
+    //         println!("{}", x);
+    //     }
+    // };
 }
