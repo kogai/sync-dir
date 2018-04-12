a9edf73391a246a4f039183cc2a0357835bca817
Shinichi Kogai <kogai0121@gmail.com>
2018-04-12 23:32:05 +0900

    Modify interface of CLI

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ mod difference;
 mod history;
 
 // TODO: Sync with Cagot.toml
-const NAME: &str = "syncdir";
+const NAME: &str = "sync-dir";
 const VERSION: &str = "0.0.1";
 
 fn main() {
@@ -22,35 +22,34 @@ fn main() {
         .version(VERSION)
         .about("Synchronize directories bidirectional")
         .arg(
-            Arg::with_name("directories")
-                .help("Set two directories you want to synchronize")
-                .short("d")
+            Arg::with_name("dir_a")
+                .help("Set directory [a] you want to synchronize")
                 .index(1)
-                .takes_value(true)
-                .multiple(true),
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("dir_b")
+                .help("Set directory [b] you want to synchronize")
+                .index(2)
+                .takes_value(true),
         )
         .get_matches();
 
-    let directories = values_t!(matches.values_of("directories"), String).unwrap_or(vec![]);
-    match directories.len() {
-        0 | 1 => println!("Number of directory you've passed are not enough"),
-        2 => {
-            println!("Directories {:?}", directories);
+    let dir_a = value_t!(matches.value_of("dir_a"), String).unwrap();
+    let dir_b = value_t!(matches.value_of("dir_b"), String).unwrap();
+    println!("Directories {:?} {:?}", &dir_a, &dir_b);
 
-            let a_path = Path::new(directories.get(0).unwrap()).to_owned();
-            let b_path = Path::new(directories.get(1).unwrap()).to_owned();
-            let a_history = history::History::new(a_path);
-            let b_history = history::History::new(b_path);
-            println!("{:?}", a_history);
-            println!("{:?}", b_history);
+    let a_path = Path::new(&dir_a).to_owned();
+    let b_path = Path::new(&dir_b).to_owned();
+    let a_history = history::History::new(a_path);
+    let b_history = history::History::new(b_path);
+    println!("{:?}", a_history);
+    println!("{:?}", b_history);
 
-            let diff_a = difference::collect_diff(&a_history, &b_history);
-            let diff_b = difference::collect_diff(&b_history, &a_history);
-            println!("{:?}", &diff_a);
-            println!("{:?}", &diff_b);
-            diff_a.iter().for_each(|diff| diff.sync_file());
-            diff_b.iter().for_each(|diff| diff.sync_file());
-        }
-        _ => println!("Number of directory you've passed are too much"),
-    };
+    let diff_a = difference::collect_diff(&a_history, &b_history);
+    let diff_b = difference::collect_diff(&b_history, &a_history);
+    println!("{:?}", &diff_a);
+    println!("{:?}", &diff_b);
+    diff_a.iter().for_each(|diff| diff.sync_file());
+    diff_b.iter().for_each(|diff| diff.sync_file());
 }
