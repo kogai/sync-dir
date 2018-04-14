ad61aada751fa7d5fc0b64621c65d1b4f9a542bd
Shinichi Kogai <kogai0121@gmail.com>
2018-04-14 11:59:06 +0900

    Sync directories when USB devices added

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
-use clap::{App, Arg};
-use libudev::{Context, Monitor};
+use difference::collect_diff;
+use history::History;
+use libudev::{Context, EventType, Monitor};
 use std::path::Path;
 use std::thread;
 use std::time::Duration;
@@ -8,9 +9,9 @@ pub fn listen() {
     let context = Context::new().unwrap();
     let mut monitor = Monitor::new(&context).unwrap();
 
-    let _ = monitor.match_subsystem_devtype("usb", "usb_device");
     // TODO: Examine whether possible to mount device via bluetooth
     let _ = monitor.match_subsystem_devtype("bluetooth", "link");
+    let _ = monitor.match_subsystem_devtype("usb", "usb_device");
     let mut socket = monitor.listen().unwrap();
 
     loop {
@@ -22,13 +23,24 @@ pub fn listen() {
             }
         };
 
-        println!(
-            "{}: {} {} (sysname={}, devtype={})",
-            event.sequence_number(),
-            event.event_type(),
-            event.syspath().to_str().unwrap_or("---"),
-            event.sysname().to_str().unwrap_or(""),
-            event.devtype().map_or("", |s| s.to_str().unwrap_or(""))
-        );
+        match event.event_type() {
+            EventType::Add => {
+                println!("Syncing...");
+                let a_path = Path::new("fixture/a").to_owned();
+                let b_path = Path::new("fixture/b").to_owned();
+                let a_history = History::new(a_path);
+                let b_history = History::new(b_path);
+
+                let diff_a = collect_diff(&a_history, &b_history);
+                let diff_b = collect_diff(&b_history, &a_history);
+
+                diff_a.iter().for_each(|diff| diff.sync_file());
+                diff_b.iter().for_each(|diff| diff.sync_file());
+                println!("Synchronounced");
+            }
+            // NOTE: It might be better to consider whether to handle a case when a user reject some device while syncing directories
+            // EventType::Remove => {}
+            _ => {}
+        };
     }
 }
