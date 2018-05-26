e361824609c48b2e6fe82891c09d498876567522
Shinichi Kogai <kogai0121@gmail.com>
2018-05-26 11:21:03 +0900

    Add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+URL=$(curl https://api.github.com/repos/kogai/sync-dir/releases/latest | grep 'browser_download_url' | grep -i "$(uname)" | cut -d\" -f4)
+FILE=$(echo $URL | sed 's/.*\(sync-dir-.*\).tar.gz/\1/')
+
+curl -L "$URL" > "/tmp/$FILE.tar.gz"
+mkdir -p "/tmp/$FILE"
+tar -xvf "/tmp/$FILE.tar.gz" -C "/tmp/$FILE"
+mkdir -p ~/bin
+mv "/tmp/$FILE/bin/$(uname)/sync-dir" ~/bin
